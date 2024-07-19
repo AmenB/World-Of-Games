e53ca43460b286487935caac1bb5a1b4c4a1857d
Amen Barakat <amen.brakat@gmail.com>
2024-07-19 13:16:23 +0300

    changed the conditions where string cause to failure

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ def welcome():
             print("Please enter your real name\n")
 def start_play():
     while True:
-        game_number = int(input("""Please choose a game to play: 
+        game_number = input("""Please choose a game to play: 
 1. Memory Game - a sequence of numbers will appear for 1 second and you have to
 guess it back.
 2. Guess Game - guess a number and see if you chose like the computer.
-3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n"""))
-        if game_number == 1 or game_number == 2 or game_number == 3:
+3. Currency Roulette - try and guess the value of a random amount of USD in ILS\n""")
+        if game_number.isdigit and (game_number == '1' or game_number == '2' or game_number == '3'):
             break
         else:
             print("Please enter only one number from the list\n")
     while True:
-        difficulty = int(input("select a difficulty level between 1 and 5:\n"))
-        if difficulty==1 or difficulty==2 or difficulty==3 or difficulty==4 or difficulty==5:
+        difficulty = input("select a difficulty level between 1 and 5:\n")
+        if difficulty.isdigit and (difficulty=='1' or difficulty=='2' or difficulty=='3' or difficulty=='4' or difficulty=='5'):
             break
         else:
             print("Please enter only one number from the list\n")
-    if game_number == 1:
-        result = memory_game.play(difficulty)
-    elif game_number == 2:
-        result = guess_game.play(difficulty)
-    elif game_number == 3:
-        result = currency_roulette_game.play(difficulty)
+    if game_number == '1':
+        result = memory_game.play(int(difficulty))
+    elif game_number == '2':
+        result = guess_game.play(int(difficulty))
+    elif game_number == '3':
+        result = currency_roulette_game.play(int(difficulty))
     if result == True:
         score.add_score(difficulty)
     while input("Do you want to try again? press 'y' to continue or 'n' to leave: ") == "y":
