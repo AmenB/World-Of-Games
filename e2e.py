544a6d150d63d3ef394e8e1db0e907b46a1495b4
Amen Barakat <amen.brakat@gmail.com>
2024-07-20 20:04:29 +0300

    added sys.exit fix

--- e2e.py
+++ e2e.py
@@ -14,9 +14,11 @@ def test_scores_service():
 
     driver_chrome.get("http://127.0.0.1:8777/")
     score = driver_chrome.find_element(By.ID, "score")
-    if int(score.text)>=0 or int(score.text)<=1000:
+    if 0 <= int(score.text) <= 1000:
+        driver_chrome.quit()
         return True
     else:
+        driver_chrome.quit()
         return False
 
 
