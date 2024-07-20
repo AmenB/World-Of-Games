bc3bca7e343d3503d0536de4d2430c3fff17a9bb
Amen Barakat <amen.brakat@gmail.com>
2024-07-20 19:35:44 +0300

    Test - e2e.py

---
+++ e2e.py
@@ -0,0 +1,31 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import ChromiumOptions
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+def test_scores_service():
+    chrome_options = ChromiumOptions()
+
+    service = Service(ChromeDriverManager().install(), options=chrome_options)
+    driver_chrome = webdriver.Chrome(service=service)
+
+    driver_chrome.get("http://127.0.0.1:5000/")
+    score = driver_chrome.find_element(By.ID, "score")
+    if int(score.text)>=0 or int(score.text)<=1000:
+        return True
+    else:
+        return False
+
+
+def main_function():
+    if test_scores_service() == True:
+        return 0
+    else:
+        return -1
+
+
+
+
+main_function()
